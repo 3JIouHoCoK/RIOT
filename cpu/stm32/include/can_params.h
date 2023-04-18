ce0d8b0bdd0a1f1bd889da7c5691f1e90d3a8adb
3JIouHoCoK <klimevdokimov@mail.ru>
2023-04-18 10:44:09 +0700

    can added

--- cpu/stm32/include/can_params.h
+++ cpu/stm32/include/can_params.h
@@ -54,8 +54,8 @@ static const can_conf_t candev_conf[] = {
         .rx_pin = GPIO_PIN(PORT_A, 11),
         .tx_pin = GPIO_PIN(PORT_A, 12),
 #elif defined(CPU_FAM_STM32L4) || defined(CPU_FAM_STM32F4)
-        .rx_pin = GPIO_PIN(PORT_B, 8),
-        .tx_pin = GPIO_PIN(PORT_B, 9),
+        .rx_pin = GPIO_PIN(PORT_A, 11),
+        .tx_pin = GPIO_PIN(PORT_A, 12),
         .af = GPIO_AF9,
 #else
         .rx_pin = GPIO_PIN(PORT_D, 0),
